2db90aef3e58c53ecb8b0ce6b19175f50d858f45
MoXuan2020 <2809603169@qq.com>
2022-06-27 12:52:22 +0800

    Update modMain.py

--- MChinaCreatorTemplateScripts/modMain.py
+++ MChinaCreatorTemplateScripts/modMain.py
@@ -2,6 +2,8 @@
 
 from mod.common.mod import Mod
 
-@Mod.Binding()
+from config import modConfig
+
+@Mod.Binding(modConfig.ModName, modConfig.ModVersion)
 class MChinaCreatorTemplateMod(object):
     pass
