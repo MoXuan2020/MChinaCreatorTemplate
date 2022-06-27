3740971cbdc9da36a3ce0639fc7e508ba33fe815
MoXuan2020 <2809603169@qq.com>
2022-06-27 13:25:58 +0800

    Update modConfig.py

--- MChinaCreatorTemplateScripts/config/modConfig.py
+++ MChinaCreatorTemplateScripts/config/modConfig.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# 模组基本信息
+"""
+这个文件存储了字符常量
+"""
+
+# 模组信息
 ModName = "MChinaCreatorTemplateMod"
 """
 模组名称
@@ -8,4 +12,24 @@ ModName = "MChinaCreatorTemplateMod"
 ModVersion = "0.0.1"
 """
 模组版本
+"""
+
+# 服务端信息
+ServerSystemName = "MChinaCreatorTemplateServerSystem"
+"""
+服务端名称
+"""
+ServerSystemClsPath = "MChinaCreatorTemplateScripts.server.modServer.MChinaCreatorTemplateServerSystem"
+"""
+服务端路径
+"""
+
+# 客户端信息
+ClientSystemName = "MChinaCreatorTemplateClientSystem"
+"""
+客户端名称
+"""
+ClientSystemClsPath = "MChinaCreatorTemplateScripts.client.modClient.MChinaCreatorTemplateClientSystem"
+"""
+服务端路径
 """
