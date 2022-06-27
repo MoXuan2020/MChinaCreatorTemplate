4ae24617bc617bf5db337f5e76de14720c0d3a76
MoXuan2020 <2809603169@qq.com>
2022-06-27 13:07:38 +0800

    Update modMain.py

--- MChinaCreatorTemplateScripts/modMain.py
+++ MChinaCreatorTemplateScripts/modMain.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import mod.server.extraServerApi as serverApi
+import mod.client.extraClientApi as clientApi
+
 from mod.common.mod import Mod
 
 from .config import modConfig
 
 @Mod.Binding(modConfig.ModName, modConfig.ModVersion)
 class MChinaCreatorTemplateMod(object):
-    pass
+    
+    @Mod.InitServer()
+    def InitMChinaCreatorTemplateServerSystem(self):
+        serverApi.RegisterSystem()
+    
+    @Mod.InitClient()
+    def InitMChinaCreatorTemplateClientSystem(self):
+        clientApi.RegisterSystem()
